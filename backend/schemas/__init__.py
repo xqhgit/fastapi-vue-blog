4dc411b4319cb2af90dd9df41b456527bccc8838
徐工Worker <1104440778@qq.com>
2020-08-06 16:42:55 +0800

    backend 增加登录接口

--- backend/schemas/__init__.py
+++ backend/schemas/__init__.py
@@ -1,1 +1,2 @@
+from .token import Token, TokenPayload
 from .user import UserCreate
---
+++ backend/schemas/token.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenPayload(BaseModel):
+    sub: Optional[int] = None
