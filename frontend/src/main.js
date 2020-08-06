4623666224916cc654ac038313f70ac5803d7098
徐工Worker <1104440778@qq.com>
2020-08-06 18:13:15 +0800

    frontend 安装Bootstrap BootstrapVue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
