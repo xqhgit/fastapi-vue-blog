b0373554eead8c1c329d6f0148a8b3827f41432a
WorkerXu <1104440778@qq.com>
2020-08-10 00:32:43 +0800

    添加markdown文本编辑器

--- frontend/src/views/manage_post/index.vue
+++ frontend/src/views/manage_post/index.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="manage-post">
     <el-page-header content="创建文章" @back="goBack"/>
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="标题">
+        <el-input v-model="form.name"/>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ManagePost',
+  data() {
+    return {
+      form: {
+        title: undefined
+      }
+    }
+  },
   methods: {
     goBack() {
       this.$router.go(-1)
