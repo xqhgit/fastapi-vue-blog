5d3523dec30013d6e424695ee4980ab9b378b55e
WorkerXu <1104440778@qq.com>
2021-07-26 23:31:00 +0800

    修改表单

--- webui/src/views/LoginPage/Index.vue
+++ webui/src/views/LoginPage/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-flex justify-content-center">
-    <b-form v-if="show" style="width: 500px;" @submit="onSubmit" @reset="onReset">
+    <b-form style="width: 500px;" @submit="onSubmit" >
       <b-form-group
         id="input-group-1"
         label="账号:"
@@ -8,8 +8,8 @@
       >
         <b-form-input
           id="input-1"
-          v-model="form.email"
-          type="email"
+          v-model="form.username"
+          type="text"
           required
         />
       </b-form-group>
@@ -17,7 +17,8 @@
       <b-form-group id="input-group-2" label="密码:" label-for="input-2">
         <b-form-input
           id="input-2"
-          v-model="form.name"
+          v-model="form.password"
+          type="password"
           required
         />
       </b-form-group>
@@ -31,32 +32,15 @@ export default {
   data() {
     return {
       form: {
-        email: '',
-        name: '',
-        food: null,
-        checked: []
-      },
-      foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-      show: true
+        username: '',
+        password: ''
+      }
     }
   },
   methods: {
     onSubmit(evt) {
       evt.preventDefault()
       alert(JSON.stringify(this.form))
-    },
-    onReset(evt) {
-      evt.preventDefault()
-      // Reset our form values
-      this.form.email = ''
-      this.form.name = ''
-      this.form.food = null
-      this.form.checked = []
-      // Trick to reset/clear native browser form validation state
-      this.show = false
-      this.$nextTick(() => {
-        this.show = true
-      })
     }
   }
 }
