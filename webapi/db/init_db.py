72eb4a7237b51b9e0c824aa5131bde140fd25459
xuqihui <1104440778@qq.com>
2021-08-17 18:28:14 +0800

    添加模型

---
+++ webapi/db/init_db.py
@@ -0,0 +1,13 @@
+import asyncio
+from webapi.db.config import engine, Base
+
+
+async def init_table():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(init_table())
---
+++ webapi/db/models/comment.py
@@ -0,0 +1,27 @@
+import datetime
+
+from sqlalchemy import Text, Integer, Column, String, DateTime, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+
+from webapi.db.config import Base
+
+
+class Comment(Base):
+    __tablename__ = 'Comment'
+
+    id = Column(Integer, primary_key=True)
+    timestamp = Column(DateTime, default=datetime.datetime.now, index=True)
+    from_admin = Column(Boolean, default=False)  # 用于判断是否为管理员的评论
+    reviewed = Column(Boolean, default=False)  # 用于判断评论是否通过审核
+
+    author = Column(String(30))
+    email = Column(String(254))
+    body = Column(Text)
+
+    replied_id = Column(Integer, ForeignKey('comment.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+    post = relationship('Post', back_populates='comments')
+    replies = relationship('Comment', back_populates='replied', cascade='all, delete-orphan')
+    replied = relationship('Comment', back_populates='replies', remote_side=[id])
+
