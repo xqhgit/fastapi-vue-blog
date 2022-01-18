b20a3b45c433cf157be6ffa0a0e2a45c1f2e0d2b
xuqihui <1104440778@qq.com>
2022-01-18 16:54:21 +0800

    1.尝试修复MySQL Operation timed out。MySQL影响参数是interactive_timeout和wait_timeout

--- webapi/db/config.py
+++ webapi/db/config.py
@@ -5,7 +5,7 @@ from webapi.setting import settings
 
 DATABASE_URL = settings.SQLALCHEMY_DATABASE_URI
 
-engine = create_async_engine(DATABASE_URL, future=True, pool_pre_ping=True)
+engine = create_async_engine(DATABASE_URL, future=True, pool_pre_ping=True, pool_recycle=3600)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 Base = declarative_base()
